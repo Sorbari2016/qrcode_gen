4f7e6ceebbe0023bde3df2868e4674961d877a9b
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-07-31 07:03:34 +0100

    Completed the qr generator mini-project

--- src/index.js
+++ src/index.js
@@ -1,73 +1,33 @@
 // ENTRY FILE
 
-// Modular Imports
-import homepage from "./module.js"
-import { loadBookTab } from "./module2.js";
-import { loadMenuTab } from "./module3.js";
+// Imports
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs'; 
 
-// Styles & Framework imports
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap'; 
-import './styles.css';
-
-
-// Clear the content area: 
-function clearPageContent() {
-    const content = document.getElementById("content"); 
-      
-    content.innerHTML = "";
-}
-
-// Remove the active class from all tab buttons
-function clearActiveTab() {
-    document.querySelectorAll('.tab').forEach(tab => {
-        tab.classList.remove('active');
-    });
-}
-
-
-
-function switchTabs() {
-    // For the home button
-    const homeTab = document.getElementById("homeTab"); 
-    homeTab.addEventListener("click", function(e){
-        e.preventDefault(); 
-        clearPageContent(); 
-        clearActiveTab();
-        e.target.classList.add('active');
-        homepage(); 
-    }); 
-
-    // For the Book button
-    const bookTab = document.getElementById("bookTab"); 
-    bookTab.addEventListener("click", function(e) {
-        e.preventDefault(); 
-        clearPageContent();
-        clearActiveTab();
-        e.target.classList.add('active'); 
-        loadBookTab(); 
-    }); 
-
-    const menuTab = document.getElementById("menuTab"); 
-    menuTab.addEventListener("click", function(e){
-        e.preventDefault(); 
-        clearPageContent();
-        clearActiveTab();
-        e.target.classList.add('active'); 
-        loadMenuTab(); 
+inquirer
+  .prompt([
+    {
+        message: 'Type in your URL', 
+        name: 'URL', 
+        type: 'input',
+    },
+    
+  ])
+  .then((answers) => {
+    const url = answers.URL
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    fs.writeFile("URL.txt", answers.URL, (err) => {
+        if (err) throw err; 
+        console.log('The file has been saved ! ')
     })
 
-}
-  
-
-// Run the homepage and swichtabs functions. 
-homepage(); 
-switchTabs(); 
-
-
-
-
-
-
-
-
+   })
+  .catch((error) => {
+    if (error.isTtyError) {
+      
+    } else {
+      
+    }
+  });
